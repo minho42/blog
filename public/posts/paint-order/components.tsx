d0c9e8969fb5907990b33419586909ce4671b01b
Min ho Kim <minho42@gmail.com>
2024-04-28 00:45:01 +1000

    new: paint-order

---
+++ public/posts/paint-order/components.tsx
@@ -0,0 +1,20 @@
+export { Tweet } from "react-tweet"
+
+export function TextWithPaintOrder({ text }) {
+  const styles = {
+    WebkitTextStrokeColor: "black",
+    WebkitTextStrokeWidth: "2px",
+    paintOrder: "stroke fill",
+    textShadow: "1px 2px black",
+  }
+
+  return (
+    <div
+      className="text-center text-6xl font-bold 
+    text-amber-500 drop-shadow-md"
+      style={styles}
+    >
+      {text}
+    </div>
+  )
+}
---
+++ public/posts/paint-order/index.md
@@ -0,0 +1,17 @@
+---
+title: TIL CSS paint-order
+date: "2024-04-27"
+---
+
+<TextWithPaintOrder text="🎨 paint-order " />
+
+```css
+/* stroke first -> fill next */
+paint-order: stroke fill;
+```
+
+[mdn](https://developer.mozilla.org/en-US/docs/Web/CSS/paint-order)
+
+---
+
+<Tweet id="1783897828508738015" />
