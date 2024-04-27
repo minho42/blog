4a780579c177a8822d3e8cd9fbe5d2441272059b
Min ho Kim <minho42@gmail.com>
2024-04-28 01:15:14 +1000

    simplify css

--- public/posts/paint-order/components.tsx
+++ public/posts/paint-order/components.tsx
@@ -1,15 +1,14 @@
 export function TextWithPaintOrder({ text }) {
   const styles = {
     WebkitTextStrokeColor: "black",
-    WebkitTextStrokeWidth: "2px",
+    WebkitTextStrokeWidth: "3px",
     paintOrder: "stroke fill",
-    textShadow: "1px 2px black",
   }
 
   return (
     <div
       className="text-center text-6xl font-bold 
-    text-amber-500 drop-shadow-md"
+    text-amber-500"
       style={styles}
     >
       {text}
