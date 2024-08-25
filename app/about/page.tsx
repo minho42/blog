c46cdf2e8d96153c0e06c0b75aad50b655d2af85
Min ho Kim <minho42@gmail.com>
2024-08-25 14:08:46 +1000

    edit about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,23 +1,31 @@
 export default function Page() {
-    return (
-      <main className="w-full sm:max-w-2xl mb-20">
-        <div className="px-4 py-1">
-            <h1 className="font-semibold text-3xl">About</h1>
-            <div className="text-sm text-neutral-500">Min ho Kim</div>
-        </div>
+  return (
+    <main className="w-full sm:max-w-2xl mb-20">
+      <div className="px-4 py-1">
+        <h1 className="font-semibold text-3xl">About</h1>
+        <div className="text-sm text-neutral-500">Min ho Kim</div>
+      </div>
 
-        <article className="space-y-1 px-4 py-2">
-          <ul>
-            <li>Registered nurse in Sydney</li>
-            <li>Aspiring indie hacker</li>
-            
-            <ul className="flex text-sm text-neutral-500 gap-2 mt-6">
-              <li className="underline"><a href="https://github.com/minho42" target="_blank" rel="noopener noreferrer">GitHub</a></li>
-              <li>·</li>
-              <li className="underline"><a href="https://www.threads.net/@_minho_" target="_blank" rel="noopener noreferrer">Threads</a></li>
-            </ul>
+      <article className="space-y-1 px-4 py-2">
+        <ul>
+          <li>Registered nurse in Sydney</li>
+          <li>Indie hacker</li>
+
+          <ul className="flex text-sm text-neutral-500 gap-2 mt-6">
+            <li className="underline">
+              <a href="https://github.com/minho42" target="_blank" rel="noopener noreferrer">
+                GitHub
+              </a>
+            </li>
+            <li>·</li>
+            <li className="underline">
+              <a href="https://www.threads.net/@_minho_" target="_blank" rel="noopener noreferrer">
+                Threads
+              </a>
+            </li>
           </ul>
-        </article>
-      </main>
-    )
+        </ul>
+      </article>
+    </main>
+  )
 }
