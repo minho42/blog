bc07c0b660beb5be0e0945329fa7b9c6f7d56218
Min ho Kim <minho42@gmail.com>
2024-11-28 23:15:04 +1100

    embedded bluesky pinned post to about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -8,7 +8,27 @@ export default function Page() {
 
       <article className="space-y-1 px-4 py-2">
         <ul>
-          <li>Nurse in Sydney</li>
+          <li>
+            <blockquote
+              className="bluesky-embed"
+              data-bluesky-uri="at://did:plc:63uu3mbgijk7a6lq7n7b347x/app.bsky.feed.post/3lbv6w575u22i"
+              data-bluesky-cid="bafyreiduyjxpmrl3djdoxsqbjbhsmpjf6eckxpdehhuow7xu2kajhmvhgi"
+            >
+              <p lang="en">
+                decided to quit nursing after 12+ years of service. hoping to transition into tech as a junior
+                developer. i guess it’s going to be pretty tough.
+              </p>
+              &mdash; Min ho Kim (
+              <a href="https://bsky.app/profile/did:plc:63uu3mbgijk7a6lq7n7b347x?ref_src=embed">
+                @minho42.com
+              </a>
+              ){" "}
+              <a href="https://bsky.app/profile/did:plc:63uu3mbgijk7a6lq7n7b347x/post/3lbv6w575u22i?ref_src=embed">
+                November 27, 2024 at 10:29 AM
+              </a>
+            </blockquote>
+            <script async src="https://embed.bsky.app/static/embed.js" charSet="utf-8"></script>
+          </li>
 
           <ul className="flex text-sm text-neutral-500 gap-2 mt-6">
             <li className="underline">
